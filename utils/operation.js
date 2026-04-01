1a09d67b642d52ce3f79bc735d38f2cbf603da35
Manar 2232 <s202248240@kfupm.edu.sa>
2026-04-01 11:18:39 +0300

    TODO-1

---
+++ utils/operation.js
@@ -0,0 +1,13 @@
+export function add(numbers) {
+    return {}
+    }
+export function subtract(numbers) {
+      return {}
+    }
+
+    export function multiply(numbers) {
+      return {}
+    }
+export function divide(numbers) {
+      return {}
+    }
